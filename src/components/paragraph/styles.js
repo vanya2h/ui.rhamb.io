6a3cb3ea938ab507a41e9ea654545e91e74587c1
Ivan Koryakovtsev <beatzhitta@gmail.com>
2019-05-12 01:47:15 +0300

    feat(Paragraph, Text): Added responsive system

--- src/components/paragraph/styles.js
+++ src/components/paragraph/styles.js
@@ -1,11 +1,29 @@
-export const styles = {
+export const styles = (theme) => ({
   paragraph: {
-    fontSize: '1rem',
+    fontSize: '0.9rem',
+    [`@media screen and ${theme.breakpoints.tablet}`]: {
+      fontSize: '1rem',
+    },
+    [`@media screen and ${theme.breakpoints.desktop}`]: {
+      fontSize: '1.02rem',
+    },
+    [`@media screen and ${theme.breakpoints.tv}`]: {
+      fontSize: '1.1rem',
+    },
   },
   primary: {
-    fontSize: '1.2rem',
+    fontSize: '1.09rem',
+    [`@media screen and ${theme.breakpoints.tablet}`]: {
+      fontSize: '1.14rem',
+    },
+    [`@media screen and ${theme.breakpoints.desktop}`]: {
+      fontSize: '1.2rem',
+    },
+    [`@media screen and ${theme.breakpoints.tv}`]: {
+      fontSize: '1.23rem',
+    },
   },
   light: {
     opacity: '0.7',
   },
-};
+});
--- src/components/text/styles.js
+++ src/components/text/styles.js
@@ -1,8 +1,17 @@
-export const styles = {
+export const styles = (theme) => ({
   text: {
-    fontSize: '1rem',
+    fontSize: '0.9rem',
+    [`@media screen and ${theme.breakpoints.tablet}`]: {
+      fontSize: '1rem',
+    },
+    [`@media screen and ${theme.breakpoints.desktop}`]: {
+      fontSize: '1.02rem',
+    },
+    [`@media screen and ${theme.breakpoints.tv}`]: {
+      fontSize: '1.1rem',
+    },
   },
   light: {
     opacity: '0.7',
   },
-};
+});
